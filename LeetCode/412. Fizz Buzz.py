183771f76aae818e0974240b1b137df0a761ef9e
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2022-01-05 18:19:12 +0300

    Create 412. Fizz Buzz.py

---
+++ LeetCode/412. Fizz Buzz.py
@@ -0,0 +1,20 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        s=[]
+        for i in  range(1,n+1):
+            if i%3==0:
+                if i%15==0:
+                    s.append("FizzBuzz")
+                else:
+                    s.append("Fizz")
+            elif i%5==0:
+                if i%15==0:
+                    s.append("FizzBuzz")
+                else:
+                    s.append("Buzz")
+            else:
+                s.append(str(i))
+        
+        return s
+            
+        
