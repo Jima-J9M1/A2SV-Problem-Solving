1f7df585e9b356802e72fc5961ca40a081a3ff36
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2022-12-24 09:37:53 +0300

    Time: 26 ms (97.50%), Space: 13.9 MB (56.60%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -2,19 +2,18 @@ class Solution:
     def interpret(self, command: str) -> str:
         
         #store the command in hashmap
-        hash_command = {"G":"G", "()":"o","(al)":"al"}
         stack_command = []
         ans = ""
         for i in range(len(command)):
             if command[i] == "G":
-                ans += hash_command[command[i]]
+                ans += "G"
             elif command[i] == "(":
                 stack_command.append(command[i])
             elif stack_command and command[i] == ")":
                 if stack_command.pop() == "(":
-                    ans += hash_command["()"]
+                    ans += "o"
                 else:
-                    ans += hash_command["(al)"]
+                    ans += "al"
             else:
                 stack_command.append(command[i])
         
