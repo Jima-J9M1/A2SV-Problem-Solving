5caa049e48e1715195a44e3c466d16313954b61d
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-01-10 09:10:07 +0300

    Time: 2282 ms (55.75%), Space: 72.1 MB (7.81%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.py
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -3,9 +3,40 @@ class Solution:
         
         power_of_two = []
         food_value = defaultdict(int)
-        ans = 0
-        modulo = 10**9 + 7
 
+        modulo = 10**9 + 7
+        value = 1
+        
+        #store all power of two from 1 - 2**21         
+        while value <= (2**21):
+            power_of_two.append(value)
+            value *= 2
+            
+
+            
+        
+        food_items = defaultdict(int)
+        count_good_meal = 0
+        
+        for ele in deliciousness:
+            
+            if ele in food_items:
+                count_good_meal += food_items[ele]
+                
+            for new_ele in power_of_two:
+                val = new_ele - ele
+                food_items[val] +=1 
+         
+        
+        return count_good_meal % modulo
+
+        
+'''     
+        power_of_two = []
+        food_value = defaultdict(int)
+
+        modulo = 10**9 + 7     
+        ans = 0
         for val in deliciousness:
 
             for j in range(22):
@@ -17,33 +48,10 @@ class Solution:
 
             food_value[val] += 1
 
-        return ans % modulo
-    
-    
-#         value = 2
-        
-#         while value <= (2**20):
-#             power_of_two.append(value)
-#             value *= 2
-
-            
-        
-#         food_items = defaultdict(int)
-#         count_good_meal = 0
-        
-#         for ele in deliciousness:
-            
-#             if ele in food_items:
-#                 count_good_meal += food_items[ele]
-                
-#             for new_ele in power_of_two:
-#                 val = new_ele - ele
-#                 food_items[val]+=1
-
+'''
                     
         
-                        
-#         return count_good_meal
+                               
 
 
 
