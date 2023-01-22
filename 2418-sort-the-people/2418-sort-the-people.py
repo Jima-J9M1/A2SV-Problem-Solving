62531cbf0749830d5d5d50f6c89401e1cc55883c
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-01-22 17:09:20 +0300

    Time: 532 ms (26.45%), Space: 15.7 MB (7.86%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -3,16 +3,14 @@ class Solution:
         
         max_val = max(heights)
         arr = [0] * (max_val + 1)
-        arr_names = [0] * (max_val + 1)
         
         for index in range(len(heights)):
-            arr[heights[index]] = 1
-            arr_names[heights[index]] = names[index]
+            arr[heights[index]] = names[index]
             
         res = []
         for index in range(len(arr) - 1, -1,-1):
-            if arr[index] > 0:
-                res.append(arr_names[index])
+            if arr[index]:
+                res.append(arr[index])
                 
         
            
