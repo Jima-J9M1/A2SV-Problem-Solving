6b9ff923d7a627a960af06aad0044b903f996739
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-01-25 15:16:33 +0300

    Time: 218 ms (31.92%), Space: 15.4 MB (5.84%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,18 +1,37 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
-        
-        num_set = set()
+        _dict = {}
+        index = 0
         length = len(numbers)
-        left = 0
         
-        while left < length:
-            if target - numbers[left] in num_set:
+        while index < length:
+            if target - numbers[index] in _dict:
                 break
+                
+            _dict[numbers[index]] = index
+            index += 1
+        
+        val = _dict[target - numbers[index]]
+        
+        return [val + 1, index + 1]
+        
+        
+        
+        
+        
+        
+#         num_set = set()
+#         length = len(numbers)
+#         left = 0
+        
+#         while left < length:
+#             if target - numbers[left] in num_set:
+#                 break
             
-            num_set.add(numbers[left])
-            left += 1
+#             num_set.add(numbers[left])
+#             left += 1
         
-        indx = numbers.index(target - numbers[left])
+#         indx = numbers.index(target - numbers[left])
         
-        return [indx + 1, left+ 1]
+#         return [indx + 1, left+ 1]
