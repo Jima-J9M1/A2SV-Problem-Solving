438fa9158aa0ef5529912a1a3e9a1b3a11d4a283
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-02-18 00:01:10 +0300

    Time: 38 ms (46.26%), Space: 13.8 MB (55.37%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        
+        
+        str_val = str(x)
+        if x < 0:
+            new_val = [str(x) for x in str_val[1:]]
+            new_val = -int("".join(new_val[::-1]))
+        else:
+            new_val = [str(x) for x in str_val]
+            new_val = int("".join(new_val[::-1]))
+        
+        
+        if (-2)**31 >= new_val or new_val >= (2**31 - 1):
+            return 0
+        
+        return int(new_val)
+            
+            
