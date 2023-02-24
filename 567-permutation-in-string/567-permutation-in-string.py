03b4a91cc7810c1eaa1b9a815282b3a11374b83c
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-02-24 14:33:01 +0300

    Time: 1280 ms (26.01%), Space: 13.8 MB (93.05%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,75 +1,22 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        ptr = len(s1)
-        _dict1 = Counter(s1)
+        len_s = len(s1)
+        count_s = Counter(s1)
+        ans = False
+        left = 0
+        right = 0
         
-        for i in range(len(s2) - ptr +1):
+        while right < len(s2):
             
-            _dict2 = Counter(s2[i:i + ptr])
-            if _dict1 == _dict2:
+            if (right - left + 1) == len(s1):
+                count_p = Counter(s2[left:right+1])
+                if count_p == count_s:
+                    ans = True
+                    break
+                left += 1
                 
-                return True
+            right += 1
+                    
+                    
+        return ans
             
-        return False
-
-#         l,r = 0,len(s2) - 1
-#         _dict = Counter(s1)
-#         res = []
-        
-#         if len(s1) == 1:
-#             if s1[-1] in s2:
-#                 return True
-#             else:
-#                 return False
-            
-#         while l < r:
-            
-#             if _dict[s2[l]] and _dict[s2[r]] and ((r - l + 1) == len(s1)):
-
-#                 while l < r:
-                    
-#                     if not _dict[s2[l]] or not _dict[s2[r]]:
-#                         return False
-                    
-#                     res.append(s2[l])
-#                     res.append(s2[r])
-                    
-                    
-#                     l += 1
-#                     r -= 1
-#                 continue
-            
-            
-#             elif _dict[s2[l]] and _dict[s2[r]] and ((r - l + 1)) > len(s1):
-#                 _dict[s2[l]] += 1
-                
-#                 while l < r and not _dict[s2[l]] or (( r - l + 1 ) != len(s1)):
-#                     if not _dict[s2[l]] -= 1:
-#                     _dict[s2[l]] -= 1
-#                     l += 1
-                    
-#                 continue
-                
-                    
-#             if l < r:
-                
-#                 if _dict[s2[l]]:
-#                     _dict[s2[l]] -= 1
-#                     r -= 1
-#                     continue
-
-#                 if _dict[s2[r]]:
-#                     _dict[s2[r]] -= 1
-#                     l += 1
-#                     continue
-                
-#                 l += 1
-#                 r -= 1
-#         st = sorted(list(s1))
-#         res.sort()
-        
-#         if st == res:
-#             return True
-        
-#         return False
-        
