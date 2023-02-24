cbf6fa8ad10a8c6ce14d06e064d4efc7a3039a4c
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-02-24 15:04:39 +0300

    Time: 69 ms (79.72%), Space: 13.8 MB (93.05%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,22 +1,33 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        len_s = len(s1)
-        count_s = Counter(s1)
-        ans = False
+        _dict = defaultdict(int)
         left = 0
         right = 0
+        len_s = len(s1)
+        count = Counter(s1)
+        ans = False
         
         while right < len(s2):
-            
-            if (right - left + 1) == len(s1):
-                count_p = Counter(s2[left:right+1])
-                if count_p == count_s:
+            if (right - left) == len_s:
+                
+                if count == _dict:
                     ans = True
                     break
+                    
+                _dict[s2[left]] -= 1
+                
+                if _dict[s2[left]] == 0:
+                    del _dict[s2[left]]
+                         
                 left += 1
                 
-            right += 1
-                    
-                    
-        return ans
+            _dict[s2[right]] += 1
             
+            right += 1
+            
+        if (right - left) == len_s:
+            if count == _dict:
+                ans = True
+            
+        
+        return ans
