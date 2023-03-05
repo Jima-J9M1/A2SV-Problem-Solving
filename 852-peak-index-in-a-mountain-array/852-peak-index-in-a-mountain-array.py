aa8caba98000fc287727fcb4bb4e2cde790c9cc4
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-03-05 22:15:07 +0300

    Time: 660 ms (25.57%), Space: 27.8 MB (29.37%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -3,15 +3,20 @@ class Solution:
         left = 0
         right = len(arr)
         
+        
+        #point the first position and last positoin of the mountain find the middle of the mountain
         while left < right:
             mid = left + (right - left) // 2
             
+            #check the peak of the moutain before and after is less than the middle one then return the peak of the mountain
             if arr[mid - 1] < arr[mid] > arr[mid + 1]:
                 return mid
             
+            #if the peak of the mountain before the middle of the mountain is greater than the middle of the mountain the change the last position to the middle one
             if arr[mid] <= arr[mid - 1]:
                 right = mid
-                
+            
+            #if the peak of the mountain after the middle of the mountain is greater than the middle of the mountain the change the last position to the middle one
             else:
                 left = mid + 1
         
