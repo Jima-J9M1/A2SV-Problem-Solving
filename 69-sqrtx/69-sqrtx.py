8d48dd13bbd9a898118e9987ead43b250a171e5a
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-03-05 22:07:29 +0300

    Time: 38 ms (72.17%), Space: 13.8 MB (93.96%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,4 +1,27 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        if x == 1:
+            return 1
         
-        return int(x**0.5)
+        left = 0
+        right = x
+        ans = 0
+        
+        # check if the sqaure root of x number is bellow middle of x or above middle of x
+        while left < right:
+            mid = left + (right - left) // 2
+            
+            #check if the middle number square is less than x number so increase our left pointer to next and add one two it
+            #store the middle value cause it maybe the answer of x number because it was written by integer
+            if x >= mid * mid:
+                left = mid + 1
+                ans = mid
+            
+            #check if the middle element is above the x number 
+            elif x < mid * mid:
+                right = mid
+            else:
+                ans = mid
+                break
+            
+        return ans
