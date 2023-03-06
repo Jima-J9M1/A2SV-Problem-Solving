76742a7ef2c45718fe5c004b3e0695293d6681bf
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-03-06 06:40:41 +0300

    Time: 641 ms (18.89%), Space: 41.8 MB (31.93%) - LeetHub

--- 2526-find-consecutive-integers-from-a-data-stream/2526-find-consecutive-integers-from-a-data-stream.py
+++ 2526-find-consecutive-integers-from-a-data-stream/2526-find-consecutive-integers-from-a-data-stream.py
@@ -1,6 +1,7 @@
 class DataStream:
 
     def __init__(self, value: int, k: int):
+        #Intaililze queue 
         self.queue = deque()
         self.k = k
         self.dic = defaultdict(int)
@@ -10,17 +11,21 @@ class DataStream:
         self.dic[num] += 1
         self.queue.append(num)
         
+        #compute if the value and lenght of the queue is equal to the given value and if true return True
         if len(self.queue) == self.k:
             if num == self.val and self.dic[num] == self.k:
                 return True
-            
+        
+        #Check if the value of the length of the queue is grreater than the given size 
         elif len(self.queue) > self.k:
             
+            #remove the element from the begining and delete from the dictonary if the val is zero
             new_val = self.queue.popleft()
             self.dic[new_val] -= 1
             if self.dic[new_val] == 0:
                 del self.dic[new_val]
             
+            #After the pop operation and delete operation check if the remaining of the queue equa with value and length of the given
             if num == self.val and self.dic[num] == self.k:
                 return True
          
