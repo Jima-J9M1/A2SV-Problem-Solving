e4aacd619de12920c1c588e2777391566004180a
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-03-09 16:04:17 +0300

    Time: 6506 ms (11.77%), Space: 13.9 MB (28.68%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -1,19 +1,18 @@
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
         
-        if len(cookies) == k:
-            return max(cookies)
+        # if len(cookies) == k:
+        #     return max(cookies)
         
-        # cookies.sort(reverse = True)
+        cookies.sort(reverse = True)
         self.minm = float("+inf")
         self.bucket = [0] * k
         self.k = k
         print(cookies)
-        self.backTracking(cookies,0)
+        self.backTracking(cookies,0, k)
         return self.minm
     
-    def backTracking(self, cookies , idx):
-        
+    def backTracking(self, cookies , idx, k):
         if idx >= len(cookies):
             self.minm = min(self.minm, max(self.bucket))
             return
@@ -21,9 +20,9 @@ class Solution:
         if self.minm < max(self.bucket):
             return 
         
-        for i in range(self.k):
+        for i in range(k):
             self.bucket[i] += cookies[idx]
-            self.backTracking(cookies,idx + 1)
+            self.backTracking(cookies,idx + 1, k)
             self.bucket[i] -= cookies[idx]
             
         return 
