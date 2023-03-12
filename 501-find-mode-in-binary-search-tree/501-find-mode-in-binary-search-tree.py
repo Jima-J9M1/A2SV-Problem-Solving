e3e238e043dff496a3c3409c87043b6d86d4048f
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-03-12 15:26:55 +0300

    Time: 69 ms (20.39%), Space: 18 MB (89.60%) - LeetHub

--- 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.py
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.py
@@ -12,26 +12,42 @@ class Solution:
         
         return mode[0]
         
-        
-    def traverse(self,curr, mode, prev, cur_freq,  freq):
+    '''
+    @param -> curr describe the current node
+    @param -> mode store the frequent value in the tree
+    @param -> prev indicate the previous node of the tree
+    @cur_freq -> the current frequency of the node
+    @freq -> maximum frequency of the node
+    '''
+    def traverse(self, curr, mode, prev, cur_freq,  freq):
         if not curr:
-            return # [Node.val, curr, maxFreq, list]
+            return 
         
         self.traverse(curr.left, mode, prev, cur_freq, freq)
+        #if the previous node is float and the current node value equal with the prevous node increase the current frequency
+        
         if prev[0] == float('-inf') or curr.val == prev[0]:
-            cur_freq[0] += 1            
+            cur_freq[0] += 1    
+            
         else:
-            # mode[0] = [curr.val]
+            #if the current node is not equal with the previous one reset the current node
             cur_freq[0] = 1
         
+        #add the current node to the mode list if the current node frequncy equal with the maximum frequncy node
         if cur_freq[0] == freq[0]:
+            
                 mode[0].append(curr.val)
-                
+        
+        #if the current node has large frequency than the maximum frquency stored  change the maximum frequency to the current frequency. Reset the mode list to the current value 
         if cur_freq[0] > freq[0]:
+            
                 mode[0] = [curr.val]
                 freq[0] = cur_freq[0]
+        
+        #change the previous node value to the current node val 
         prev[0] = curr.val
         self.traverse(curr.right, mode, prev, cur_freq, freq)
+        
         
         
         
