a094ffd1c08b20358aacfad7e3514f286dfd9d34
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-03-12 15:55:33 +0300

    Time: 45 ms (73.21%), Space: 17.1 MB (11.88%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -8,20 +8,28 @@ class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         return self.BTS(root, [float ("+inf")])
         
+    #validate binary search tree with inorder traversal
+    '''
+    @param: curr -> the current node of the tree
+    @param: prev -> the previous node of the tree
+    
+    '''
     def BTS(self, curr, prev):
-        
+        #if the current is none return true 
         if not curr:
             return True
         
         left = self.BTS(curr.left, prev)
-        
+        #the previous node different and if prevous value is greater than the current value the return false 
         if prev[0] != float ("+inf") and prev[0] >= curr.val:
             return False
-        
+        #assign the current value to the previous value
         prev[0] = curr.val
         
+        #travres to the right 
         right = self.BTS(curr.right, prev)
-                
+         
+        #return boolean value using and opperatoin
         return right and left
     
         
