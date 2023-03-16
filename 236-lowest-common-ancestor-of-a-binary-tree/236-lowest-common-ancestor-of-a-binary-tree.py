8f5a7bfb17d115592826961121f74d6b8527d93e
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-03-16 12:39:27 +0300

    Time: 66 ms (88.42%), Space: 26.2 MB (55.10%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,33 +7,54 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        descendant = [[]]
         
-        self.dfs(root, p , q, [], descendant)
-        
-        ptr1 = 0
-        ptr2 = 0
-        while (ptr1 < len(descendant[0][0]) and ptr2 < len(descendant[0][1])) and descendant[0][0][ptr1].val == descendant[0][1][ptr2].val:
-            ptr1 += 1
-            ptr2 += 1
-        
-        return descendant[0][0][ptr1 - 1]
+        return self.dfs(root, p, q)
         
         
-    def dfs(self, curr, node1, node2, anscestor,descendant):
+    def dfs(self, root,node1,node2):
+        if not root:
+            return 
         
-        if not curr or len(descendant[0]) == 2:
-            return
+        if root == node1 or root == node2:
+            return root
         
-        anscestor.append(curr)
-        if curr.val == node1.val:
-            descendant[0].append(anscestor[:])
-        elif curr.val == node2.val:
-            descendant[0].append(anscestor[:])
+        left = self.dfs(root.left, node1, node2)
+        right = self.dfs(root.right, node1, node2)
+        
+        if (left == None):
+            return right
+        if (right == None):
+            return left
+        
+        return root
+#         descendant = [[]]
+        
+#         self.dfs(root, p , q, [], descendant)
+        
+#         ptr1 = 0
+#         ptr2 = 0
+#         while (ptr1 < len(descendant[0][0]) and ptr2 < len(descendant[0][1])) and descendant[0][0][ptr1].val == descendant[0][1][ptr2].val:
+#             ptr1 += 1
+#             ptr2 += 1
+        
+#         return descendant[0][0][ptr1 - 1]
+        
+        
+#     def dfs(self, curr, node1, node2, anscestor,descendant):
+        
+#         if not curr or len(descendant[0]) == 2:
+#             return
+        
+#         anscestor.append(curr)
+#         if curr.val == node1.val:
+#             descendant[0].append(anscestor[:])
+#         elif curr.val == node2.val:
+#             descendant[0].append(anscestor[:])
             
         
-        self.dfs(curr.left,node1, node2, anscestor[:], descendant)
-        self.dfs(curr.right, node1, node2, anscestor[:], descendant)
-        anscestor.pop()
+#         self.dfs(curr.left,node1, node2, anscestor[:], descendant)
+#         self.dfs(curr.right, node1, node2, anscestor[:], descendant)
+#         anscestor.pop()
         
-        return 
+#         return 
+
