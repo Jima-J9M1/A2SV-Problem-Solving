8f053c1bd2e710788003fa2acde5d31221f72fec
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-03-21 10:11:23 +0300

    Time: 306 ms (50.31%), Space: 17.9 MB (14.62%) - LeetHub

--- 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.py
+++ 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.py
@@ -15,6 +15,10 @@ class Solution:
         right = 0
         ans = 0
         
+        '''
+        Find subarray that is equal with goal, the goal is greater than 0  
+        used a formual for consecutive numbers n(n + 1) // 2 
+        '''
         while right < len(keys) and goal != 0:
             
             if keys[right] > goal:
@@ -33,7 +37,13 @@ class Solution:
         left = 0
         right = 0
         
+        '''
+        Find subarray sum which satisfy the goal 0
+        Used a formula of consecutive number which is n(n + 1)/ 2
+        
+        '''
         while right < len(keys) and goal == 0:
+            
             
             if keys[right] > goal and _dict[keys[right]] > 1:
                 ans += ((_dict[keys[right]] - 1) * _dict[keys[right]])// 2
