fdc4aa0c7073058f45551102a845e42d5dd7286e
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-03-21 10:04:30 +0300

    Time: 297 ms (63.20%), Space: 17.8 MB (34.32%) - LeetHub

---
+++ 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.py
@@ -0,0 +1,46 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        
+        prefix_sum = [0] * len(nums)
+        prefix_sum[0] = nums[0]
+        
+        for i in range(1, len(nums)):
+            prefix_sum[i]  = prefix_sum[i - 1] + nums[i]
+            
+        _dict = Counter(prefix_sum)
+        
+        
+        keys = list(_dict.keys())
+        left = 0
+        right = 0
+        ans = 0
+        
+        while right < len(keys) and goal != 0:
+            
+            if keys[right] > goal:
+                if _dict[keys[right] - goal]:
+                    
+                    ans += (_dict[keys[right] - goal] * _dict[keys[right]])
+                    
+            elif keys[right] == goal:
+                if _dict[keys[right] - goal]:
+                    ans += (_dict[keys[right] - goal] * _dict[keys[right]])
+                    
+                ans += (_dict[keys[right]])
+            
+            right += 1
+            
+        left = 0
+        right = 0
+        
+        while right < len(keys) and goal == 0:
+            
+            if keys[right] > goal and _dict[keys[right]] > 1:
+                ans += ((_dict[keys[right]] - 1) * _dict[keys[right]])// 2
+                
+            elif keys[right] == goal:
+                ans += (_dict[keys[right]] * (_dict[keys[right]] + 1))  // 2
+            
+            right += 1
+            
+        return ans
