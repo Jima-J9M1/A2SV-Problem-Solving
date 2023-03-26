d1cbf4037463495f382d53b1dbdf6c4bfd90d21a
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-03-26 21:37:20 +0300

    Time: 6213 ms (5.00%), Space: 130 MB (6.67%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,40 +1,24 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         
-        def merge(nums1, nums2):
-            ptr1 = 0
-            ptr2 = 0
-            ans = []
-            # print(nums1, nums2)
-            while ptr1 < len(nums1) and ptr2 < len(nums2):
-                
-                if nums1[ptr1] < nums2[ptr2]:
-                    ans.append(nums1[ptr1])
-                    ptr1 += 1
+        def quickSort(arr):
+            if len(arr) <= 1:
+                return arr
+            
+            pivot = arr[0]
+            left = []
+            right = []
+            
+            for indx in range(1, len(arr)):
+                if arr[indx] < pivot:
+                    left.append(arr[indx])
                     
                 else:
-                    ans.append(nums2[ptr2])
-                    ptr2 += 1
+                    right.append(arr[indx])
                     
-            
-            if ptr1 < len(nums1):
-                ans += nums1[ptr1:]
-                
-            if ptr2 < len(nums2):
-                ans += nums2[ptr2:]
-                
-            return ans
+            return quickSort(left) + [pivot] + quickSort(right)
         
-        def mergeSort(left,right, nums):
-            if left == right:
-                return [nums[left]]
-            
-            mid = left + (right - left ) // 2 
-            
-            left = mergeSort(left, mid, nums)
-            right = mergeSort(mid + 1 , right, nums)
-            
-            return merge(left, right)
         
-        ans = mergeSort(0, len(nums)- 1, nums)
-        return ans[-k]
+        sort_val = quickSort(nums)
+        
+        return sort_val[-k]
