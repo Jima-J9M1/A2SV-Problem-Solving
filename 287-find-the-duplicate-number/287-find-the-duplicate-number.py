b6237b13ec3d2d02c64a29ffaac2a26e041d138a
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-03-27 12:35:24 +0300

    Time: 896 ms (9.35%), Space: 28.1 MB (29.61%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,7 +1,30 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        """Binary Search"""
         
-        ans = 0
+        left = 0
+        right = len(nums) - 1
+        mid = left + (right - left) // 2
+        
+        while right - left > 1:
+            count = 0
+            
+            for num in nums:
+                if mid < num <= right:
+                    count += 1
+                    
+            if count > right - mid:
+                left = mid
+            else:
+                right = mid
+            
+            mid = left + (right - left ) // 2
+        
+        return right
+        
+        
+        
+        """ans = 0
         for num in nums:
             if nums[abs(num) - 1] < 0:
                 ans = abs(num)
@@ -9,4 +32,4 @@ class Solution:
             else:
                 nums[abs(num) - 1] *= -1
                 
-        return ans
+        return ans"""
