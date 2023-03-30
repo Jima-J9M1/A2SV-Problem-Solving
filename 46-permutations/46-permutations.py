1293acaf8f19e6e63d4e66cceb42b7d26880bdac
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-03-30 15:31:58 +0300

    Time: 55 ms (6.05%), Space: 13.9 MB (76.65%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -6,26 +6,41 @@ class Solution:
         
         return result[0]
         
+        """
+        @param: nums -> input list
+        @param: visited -> it mark visited number with bit manipulation
+        @param: path -> store not visited number in the list
+        @param: result -> store permutation of the nums in the result
+        """
     def backTracking(self, nums, visited, path, result):
         if len(nums) == len(path):
             result[0].append(path[:])
             return
         
         for indx in range(len(nums)):
+            # number is negative, shift 1bit to the left 10  + current number times and store the element in that pos
             if nums[indx] < 0:
                 shift = 1 << (10 - (nums[indx]))
-                
+            
+            # number is postive, shift 1bit to the left  current number times and store the element in that pos
             else:
                 shift = 1 << nums[indx]
             
+            #check if the visited number and operator shift value gives 0 then the position is not visited 
             if visited & shift == 0:
+            
+                #assign the postion of the number to be visited
                 visited |= (shift)
                 path.append(nums[indx])
                 self.backTracking(nums, visited, path, result)
                 val = path.pop()
                 
+                #if negative, to remove shift 1bit to the left 10 times + popped value times 
+                #And do the XOR operation
+                
                 if val < 0:
                     visited ^=  1 << (10 - (val))
+               #postive, shift  1bit to the left times the poped value and do XOR operation     
                 else:
                     visited ^= 1 << val
             
