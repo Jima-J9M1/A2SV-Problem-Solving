ad6a9dd2c822ef823f32a9689fabb0150bc09936
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-03-30 15:08:15 +0300

    Time: 46 ms (31.07%), Space: 14.1 MB (49.63%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,5 +1,37 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        visited = 0
+        result = [[]]
+        self.backTracking(nums, visited, [], result)
+        
+        return result[0]
+        
+    def backTracking(self, nums, visited, path, result):
+        if len(nums) == len(path):
+            result[0].append(path[:])
+            return
+        
+        for indx in range(len(nums)):
+            if nums[indx] < 0:
+                shift = 1 << (10 - (nums[indx]))
+                
+            else:
+                shift = 1 << nums[indx]
+            
+            if visited & shift == 0:
+                visited |= (shift)
+                path.append(nums[indx])
+                self.backTracking(nums, visited, path, result)
+                val = path.pop()
+                
+                if val < 0:
+                    visited ^=  1 << (10 - (val))
+                else:
+                    visited ^= 1 << val
+            
+            
+            
+        """
         visited = set()
         result = [[]]
         self.backTracking(nums, visited, [], result)
@@ -20,6 +52,7 @@ class Solution:
                 val = path.pop()
                 visited.remove(val)
                 
-            # if len(path) == 0:
-            #     visited = set()
+                """
+                
+            
         
