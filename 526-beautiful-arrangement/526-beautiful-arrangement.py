7f0c02b5d04251ac049ab74e19417a55ce880494
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-04-06 01:09:11 +0300

    Time: 2235 ms (8.17%), Space: 13.7 MB (96.88%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.py
+++ 526-beautiful-arrangement/526-beautiful-arrangement.py
@@ -1,19 +1,23 @@
 class Solution:
     def countArrangement(self, n: int) -> int:
-        visited = set()
-        count = self.backTracking(n, visited)
+        visited = 0
+        count = self.backTracking(n, visited, 0)
         
         return count
-    def backTracking(self,n, visited):
-        if len(visited) == n:
+    
+    def backTracking(self,n, visited, bit_count):
+        if bit_count == n:
             return 1
         
         count = 0
         for i in range(1, n + 1):
-            if i not in visited and ((len(visited) + 1) % i == 0 or (i % (len(visited) + 1)) == 0):
-                visited.add(i)
-                count += self.backTracking(n, visited)
-                visited.remove(i)
+            shift = 1 << (i - 1)
+            
+            
+            if (visited & shift)== 0 and ((bit_count + 1) % i == 0 or (i % (bit_count + 1)) == 0):
+                visited |= shift
+                count += self.backTracking(n, visited, bit_count + 1)
+                visited ^= shift 
             
         return count
                 
