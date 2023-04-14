05a8cbf7c7e74d21c2c1f43b1505a7f61e01a1a9
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-04-14 05:30:03 +0300

    Time: 659 ms (99.64%), Space: 20.8 MB (43.61%) - LeetHub

---
+++ 886-possible-bipartition/886-possible-bipartition.py
@@ -0,0 +1,33 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        graph = [[] for i in range(n + 1)]
+        colors = [-1] * (n + 1)
+
+        for row in range(len(dislikes)):
+            ui, vi = dislikes[row]
+            graph[ui].append(vi)
+            graph[vi].append(ui)
+            
+        
+        for key in range(1, n + 1):
+            if colors[key] == - 1:
+                if not self.dfs(key, graph, colors, 0):
+                    return False
+        
+        return True
+    
+    
+        
+    def dfs(self, node, graph, colors, color):
+        colors[node] = color
+        
+        for child in graph[node]:
+            if colors[child] == -1:
+                if not self.dfs(child, graph, colors, 1 - color):
+                    return False
+                
+            elif colors[child] == color:
+                return False
+        
+        return True
+        
