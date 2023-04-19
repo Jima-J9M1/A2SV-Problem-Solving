1edcb9902cb0398e147531dc54e17b7f163759e3
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-04-19 07:16:33 +0300

    Time: 670 ms (5.39%), Space: 13.9 MB (70.05%) - LeetHub

---
+++ 93-restore-ip-addresses/93-restore-ip-addresses.py
@@ -0,0 +1,29 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        number_of_dot = 0
+        result = [[]]
+        self.backTracking(0, "", number_of_dot, s, result)
+        
+        return result[0]
+    def backTracking(self, indx, path, number_of_dot,s,result):
+        if len(s) < indx:
+            return
+        
+        
+        if path.count(".") == 4 and  (len(s) + 4) == len(path):
+            arr = path.split('.')
+            arr.pop()
+            flag = True
+            for ele in arr:
+                if len(ele) == 0 or int(ele) > 255:
+                    flag = False
+                elif len(ele) > 1 and ele[0] == "0":
+                    flag = False
+                           
+            if flag:    
+                result[0].append(path[:-1])
+                
+        for i in range(indx, len(s)):
+            path += s[i]
+            self.backTracking(i + 1, path + ".", number_of_dot + 1, s, result)
+            
