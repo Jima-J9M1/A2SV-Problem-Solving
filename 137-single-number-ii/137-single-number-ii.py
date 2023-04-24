e37af66041c27c19c7f0b6916d7c0148ed61595e
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-04-24 17:04:41 +0300

    Time: 117 ms (34.21%), Space: 15.7 MB (80.09%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.py
+++ 137-single-number-ii/137-single-number-ii.py
@@ -1,10 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        count = Counter(nums)
-        res = count.items()
-        ans = sorted(res, key=lambda x:x[1] )
+#         count = Counter(nums)
+#         res = count.items()
+#         ans = sorted(res, key=lambda x:x[1] )
         
-        return ans[0][0]
+#         return ans[0][0]
 #         count = defaultdict(int)
 #         shift = 0
 #         min_val = 0
@@ -33,5 +33,57 @@ class Solution:
 #             ans = shift & sh
 #             if ans > 0:
 #                 return num
+
         
-#         print(shift)
+        min_val = min(nums)
+        
+        if min_val >= 0:
+            offset = 0
+        else:
+            offset = -min_val
+        
+        for indx in range(len(nums)):
+            nums[indx] += (offset)
+        
+        res = 0
+        for i in range(32):
+            count = 0
+            for ele in nums:
+                shift = ele & (1 << i)
+                if shift > 0:
+                    count += 1
+            
+            if count % 3:
+                res += 2**(i)
+                
+        return res - offset
+            
+        
+#         for ele in nums:
+#             if ele < 0:
+#                 bit_rep += int(bin(-ele)[2:])
+                
+#             else:
+#                 pos_val = (ele - min_val) + 1
+#                 bit_rep += int(bin(pos_val)[2:])
+
+#         print(bit_rep)
+#         binary_rep = ""   
+#         while bit_rep != 0:
+            
+#             rem = (bit_rep % 10) % 3
+
+#             binary_rep = str(rem) + binary_rep
+  
+#             bit_rep //= 10
+#             print(bit_rep, binary_rep)
+            
+#         single_numb = int(binary_rep, 2)
+#         res = 0
+        
+#         if single_numb > (-min_val):
+#             res = (single_numb + min_val) - 1
+#         else:
+#             res = -single_numb
+            
+#         return res
