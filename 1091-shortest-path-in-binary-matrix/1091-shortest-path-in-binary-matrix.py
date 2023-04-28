b25387b0a340cda717ae0f1e09dd3568900a1c1c
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-04-28 19:51:59 +0300

    Time: 673 ms (43.09%), Space: 16.8 MB (5.12%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,14 +1,16 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        visited = [[1]*len(grid[0]) for i in range(len(grid))]
 
         directions = [[1,1],[0,1],[1,0],[-1,1],[1,-1],[0,-1],[-1,0],[-1,-1]]
         level = 1
         queue = deque([[0,0]])
+        
         n = len(grid)
         
         if grid[0][0] == 1 or grid[n - 1][n - 1] == 1:
             return -1
+        
+        grid[0][0] = 1
         
         def isbound(grid, row, col):
             return ((0 <= row < len(grid) ) and (0 <= col < len(grid[0])))
@@ -33,10 +35,10 @@ class Solution:
                     
                    
                         
-                    if isbound(grid, new_row, new_col) and visited[new_row][new_col] and grid[new_row][new_col] == 0:
+                    if isbound(grid, new_row, new_col)  and grid[new_row][new_col] == 0:
                         
                         queue.append([new_row, new_col])
-                        visited[new_row][new_col] = False
+                        grid[new_row][new_col] = 1
                         
                         
                         
