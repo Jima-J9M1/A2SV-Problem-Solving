bbf6013d167e254fad518d1b0a95e5a1f7e278e5
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-05-17 20:09:33 +0300

    Time: 2450 ms (12.56%), Space: 108.9 MB (50.55%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,56 +1,38 @@
 class Solution:
-    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        graph = defaultdict(list)
-        visited = set()
+    def find(self, node):
+        if node == self.rep[node]:
+            return node
         
-        for edge in edges:
-            vertex_1, vertex_2 = edge
-            graph[vertex_1].append(vertex_2)
-            graph[vertex_2].append(vertex_1)
-        
-        
-        # ans = self.dfs(graph, visited, source, destination )
-        ans = self.iterative(graph, visited, source, destination)
-        
-        return ans
-        
-    def dfs(self, graph, visited, vertex, destination):
-        if vertex == destination:
-            return True
-        
-        visited.add(vertex)
+        return self.find(self.rep[node])
         
         
         
-        for neighbour in graph[vertex]:
-            
-            if neighbour not in visited:
-                found = self.dfs(graph, visited, neighbour, destination)
-                
-                if found:
-                    return True
-                
-                
-        return False
     
-    def iterative(self, graph, visited, vertex, destination):
-        stack = [vertex]
+    def union(self, start, dest):
+        parent_start = self.find(start)
+        parent_des = self.find(dest)
         
+        if parent_start == parent_des:
+            return
         
-        while stack:
-            node = stack.pop()
-            
-            visited.add(node)
-            
-            if node == destination:
-                return True
-            
-            for neighbour in graph[node]:
-                
-                if neighbour not in visited:
-                    stack.append(neighbour)
-                    
-        return False
+        self.rep[parent_start] = parent_des
         
         
         
+    
+        
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        self.rep = {i : i for i in range(n)}
+        
+        for start, end in edges:
+            self.union(start,end)
+        
+        parent_source = self.find(source)
+        parent_des = self.find(destination)
+        
+        return parent_source == parent_des
+        
+        
+        
+        
+    
