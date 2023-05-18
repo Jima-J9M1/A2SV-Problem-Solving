9ef204c7557ba59a8eaf948247aeef4d8b25af8c
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-05-18 22:00:17 +0300

    Time: 214 ms (18.55%), Space: 16.8 MB (22.33%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,5 +1,50 @@
 class Solution:
+    def find(self, city):
+        if city == self.rep[city]:
+            return city
+        
+        ret = self.find(self.rep[city])
+        self.rep[city] = ret
+        return ret
+    
+    
+    def union(self, city1,city2):
+        rep_city1 = self.find(city1)
+        rep_city2 = self.find(city2)
+        
+        if rep_city1 != rep_city2:
+            if self.size[rep_city1] > self.size[rep_city2]:
+                self.rep[rep_city2] = rep_city1
+                
+            elif self.size[rep_city1] < self.rep[rep_city2]:
+                self.rep[rep_city1] = rep_city2
+                
+            else:
+                self.rep[rep_city1] = rep_city2
+                self.size[rep_city2] += 1
+                
+                
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        length = len(isConnected)
+        self.size = [0] * length
+        self.rep = {i : i for i in range(length)}
+        
+        for indx, city in enumerate(isConnected):
+            for i,ele in enumerate(city):
+                if ele == 1:
+                    self.union(indx,i)
+        result = set()       
+        for indx in range(len(isConnected)):
+            result.add(self.find(indx))
+            
+        return len(result)
+        
+        
+        
+        
+        
+        
+        '''
         graph = defaultdict(list)
         visited = set()
         count = 0
@@ -30,6 +75,8 @@ class Solution:
 
         
         return count
+        
+        '''
          
             
         
