50d13c918e9774ac519b7ecd1553db024e806b6f
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-05-18 21:11:46 +0300

    Time: 2060 ms (50.13%), Space: 108.8 MB (53.10%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -14,10 +14,22 @@ class Solution:
         parent_start = self.find(start)
         parent_des = self.find(dest)
         
-        if parent_start == parent_des:
-            return
         
-        self.rep[parent_start] = parent_des
+        if parent_start != parent_des:
+            if self.size[parent_start] > self.size[parent_des]:
+                self.rep[parent_des] = parent_start
+                
+            elif self.size[parent_start] < self.size[parent_des]:
+                self.rep[parent_start] = parent_des
+                
+            else:
+                self.rep[parent_des] = parent_start
+                self.size[parent_start] += 1
+                     
+#         if parent_start == parent_des:
+#             return
+        
+#         self.rep[parent_des] = parent_start'
         
         
         
@@ -25,6 +37,7 @@ class Solution:
         
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         self.rep = {i : i for i in range(n)}
+        self.size = [0] * n
         
         for start, end in edges:
             self.union(start,end)
