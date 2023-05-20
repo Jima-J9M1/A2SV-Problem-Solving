5e6aaf70e4b13c6ede10da31b81fe335fcfdeeef
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-05-20 08:54:51 +0300

    Update Graph_without_long_directed_paths.py

--- Graph_without_long_directed_paths/Graph_without_long_directed_paths.py
+++ Graph_without_long_directed_paths/Graph_without_long_directed_paths.py
@@ -1,69 +1,48 @@
-from collections import deque,Counter,defaultdict
 import heapq
-
+from collections import deque, defaultdict
 import math
  
 def II():return int(input())
 def LI(): return list(map(int,input().split()))
+def LSI(): return list(map(str,input().split()))
 def MI(): return map(int,input().split())
 def SI(): return sorted(list(map(int,input().split())))
  
 def solve():
-    
-    edge,k = MI()
-    # print(edge)
+    V,E = MI()
     graph = defaultdict(list)
-    incoming = defaultdict(int)
-    queue = deque()
-    visited = set()
+    edge = []
 
-
-    for _ in range(edge - 1):
+    for i in range(E):
         v1, v2 = MI()
+        edge.append((v1,v2))
         graph[v1].append(v2)
         graph[v2].append(v1)
-        incoming[v1] += 1
-        incoming[v2] += 1
+    
+    visited = [0] * (V + 1)
+    stack = [(1,1)]
 
-    # print(graph)
-    for ele in range(1, edge + 1):
-        if incoming[ele] == 0 or incoming[ele] == 1:
-            queue.append(ele)
-            visited.add(ele)
-
-    ans = 0
- 
-    while queue and k > 0:
-        len_level = len(queue)
-        ans += len_level 
-
-
+    while stack:
+        node, color = stack.pop()
+        visited[node] = color
         
-        for __ in range(len_level):
-            
-            node = queue.popleft()
-            
-            for neighbor in graph[node]:
-                if neighbor not in visited:
-                    incoming[neighbor] -= 1
 
-                    if incoming[neighbor] == 1:
-                        queue.append(neighbor)
-                        visited.add(neighbor)
-
-
-        k -= 1
-
-    res = edge - ans 
-
-    print(res)
-            
+        for neighbor in graph[node]:
+            if visited[neighbor] == 0:
+                stack.append((neighbor,-color))
+                
+            else:
+                if visited[neighbor] == color:
+                    print("NO")
+                    return
+    print("YES")
+    for start,end in edge:
+        print(1 if visited[start] == 1 else 0, end = "")
 
 
-tastCase = II()
+solve()
+                
 
-for __ in range(tastCase):
-    input()
-    solve()
-
-  
+     
+                
+                
