3c92e37557c419f9f93d5cb64f5db0208f52bf6b
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-05-25 15:16:58 +0300

    Time: 56 ms (39.66%), Space: 16.5 MB (10.42%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -0,0 +1,46 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        difference_arr = []
+        
+        for left, right in costs:
+            diff = left - right
+            
+            difference_arr.append([diff, left, right])
+        
+        sorted_arr = self.pivotSort(difference_arr)
+        
+        comp_a = 0
+        comp_b = 0
+        check = True
+        for indx in range(len(sorted_arr)//2):
+            comp_a += sorted_arr[indx][1]
+            
+        for indx in range(len(sorted_arr)//2, len(sorted_arr)):
+            comp_b += sorted_arr[indx][2]
+        
+        
+        return comp_a + comp_b
+          
+            
+            
+            
+        
+        
+        print(sort_arr)
+        
+    def pivotSort(self,nums):
+        if len(nums) <= 1:
+            return nums
+        
+        pivot = nums[0]
+        left = []
+        right = []
+        
+        for i in range(1, len(nums)):
+            if nums[i] > pivot:
+                right.append(nums[i])
+            else:
+                left.append(nums[i])
+                
+                
+        return self.pivotSort(left) + [pivot] + self.pivotSort(right)
