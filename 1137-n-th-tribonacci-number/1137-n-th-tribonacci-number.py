02c48e692178fa9cbe9c4393b28ccf7763f57de5
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-05-28 22:11:49 +0300

    Time: 51 ms (13.28%), Space: 16.4 MB (10.01%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,12 +1,35 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        tribonacci = {0:0}
-        for i in range(1, n + 1):
-            if i <= 2:
-                trib = 1
-            else:
-                trib = tribonacci[i - 1] + tribonacci[i - 2] + tribonacci[i - 3]
+        memo = {}
+        result = self.trib(n,memo)
+        
+        return result
+    
+    
+    def trib(self,n,memo):
+        if n == 0:
+            return 0
+        if n <= 2:
+            return 1
+        
+        if n in memo:
+            return memo[n]
+        
+        t = self.trib(n - 1, memo) + self.trib(n - 2, memo) + self.trib(n - 3, memo)
+        memo[n] = t
+        
+        return memo[n]
+        
+        
+        
+#         BOTTOM UP APPROACH 
+#         tribonacci = {0:0}
+#         for i in range(1, n + 1):
+#             if i <= 2:
+#                 trib = 1
+#             else:
+#                 trib = tribonacci[i - 1] + tribonacci[i - 2] + tribonacci[i - 3]
                 
-            tribonacci[i] = trib
+#             tribonacci[i] = trib
             
-        return tribonacci[n]
+#         return tribonacci[n]
