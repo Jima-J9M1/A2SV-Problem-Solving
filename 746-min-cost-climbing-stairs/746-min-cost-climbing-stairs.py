74ec384cefdf5acab55e426494e67097af2558bd
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-06-01 19:24:59 +0300

    Time: 78 ms (18.85%), Space: 16.5 MB (33.26%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,33 +1,39 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        memo = {}
-        ans = self.dp(-1, cost, memo)
+        dp = [0] * (len(cost) + 2)
+        dp[-3] = cost[-1]
+        for i in range(len(cost) - 1, -1 , -1):            
+            dp[i] = min(dp[i + 1], dp[i + 2]) + cost[i]
+        return min(dp[0], dp[1])
         
-        return ans
+#         memo = {}
+#         ans = self.dp(-1, cost, memo)
         
-    def dp(self, indx, cost, memo):
-        if indx not in memo:
+#         return ans
+        
+#     def dp(self, indx, cost, memo):
+#         if indx not in memo:
             
-            if len(cost) <= indx:
-                return 0
+#             if len(cost) <= indx:
+#                 return 0
 
 
-            first = 0
-            second = 0
+#             first = 0
+#             second = 0
 
-            if (indx + 2) < len(cost):
-                first = cost[indx + 2] + self.dp(indx + 2, cost, memo)
+#             if (indx + 2) < len(cost):
+#                 first = cost[indx + 2] + self.dp(indx + 2, cost, memo)
                 
 
 
-            if (indx + 1) < len(cost):
-                second = cost[indx + 1] + self.dp(indx + 1, cost, memo)
+#             if (indx + 1) < len(cost):
+#                 second = cost[indx + 1] + self.dp(indx + 1, cost, memo)
 
 
 
 
-            memo[indx] =  min(first, second)
+#             memo[indx] =  min(first, second)
         
-        return memo[indx]
+#         return memo[indx]
 
         
