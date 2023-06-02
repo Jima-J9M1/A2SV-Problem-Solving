0d978d856963f42fe6df987c50e7327820f1c5e5
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-06-02 19:28:35 +0300

    Time: 53 ms (14.23%), Space: 16.5 MB (17.65%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,49 +1,29 @@
 class Solution:
-    
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[-1]
         
-#         Base
+        
+        memo = {}
+        
+    
+        def dfs(indx):
+            if indx not in memo:
+                if indx >= len(nums):
+                    return 0
 
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
-        dp[1] = max(nums[1], nums[0])
-         
-        curMax = dp[0]
-        
-        for i in range(2,len(nums)):
-            dp[i] = curMax + nums[i]
-            curMax = max(curMax, dp[i - 1])
+
+
+                rob = dfs(indx + 2) + nums[indx]
+                not_rob = dfs(indx + 1)
+
+
+                memo[indx] = max(rob, not_rob)
             
-        return max(dp)
-#         memo = {}
+            return memo[indx]
         
-#         ans = self.dp(nums,0,memo)
-#         return ans
-        
-#     def dp(self,nums, indx, memo):
-#         if indx >= len(nums):
-#             return 0
-        
-#         if indx == len(nums) - 1:
-#             return nums[indx]
-        
-#         for i in range(indx, len(nums)):
-#             if (i + 2) not in memo:
-#                 res = self.dp(nums, (i + 2), memo)
-#                 memo[i] = res + nums[i]
-                
-#             else:
-#                 memo[i] = memo[i + 2] + nums[i]
-                
-                
-#             if (i + 1) not in memo:
-#                 res = self.dp(nums,(i + 1), memo)
-#                 memo[i + 1] = res
             
-#             return max(memo[i], memo[i + 1])
-        
+        ans = dfs(0)
+        return ans
+    
             
             
         
