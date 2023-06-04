57090978e8b9ac721bf2deeb8c79bfce3c91ed8a
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-06-04 14:59:17 +0300

    Time: 2218 ms (27.70%), Space: 16.8 MB (62.56%) - LeetHub

--- 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.py
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.py
@@ -1,30 +1,50 @@
 class Solution:
     def findLongestChain(self, pairs: List[List[int]]) -> int:
-        memo = {}
-        
-        def dp(indx):
-            if indx == 0:
-                return 1
-            
-            if indx in memo:
-                return memo[indx]
-            
-            max_length = 1
-            for i in range(indx):
-                if pairs[indx][0] > pairs[i][1]:
-                    max_length = max(max_length, 1 + dp(i))
-                    
-                    
-            memo[indx] = max_length
-            return memo[indx]
+        dp = [1] * len(pairs)
         
         pairs.sort()
+        for i in range(1, len(pairs)):
+            for j in range(i):
+                if pairs[i][0]  > pairs[j][1]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+                    
+                    
+        return max(dp)
         
-        max_length = 1
         
-        for i in range(len(pairs)):
-            max_length = max(max_length, dp(i))
+        
+        
+        
+        
+        
+        
+        
+        
+#         memo = {}
+        
+#         def dp(indx):
+#             if indx == 0:
+#                 return 1
             
-        return max_length
+#             if indx in memo:
+#                 return memo[indx]
+            
+#             max_length = 1
+#             for i in range(indx):
+#                 if pairs[indx][0] > pairs[i][1]:
+#                     max_length = max(max_length, 1 + dp(i))
+                    
+                    
+#             memo[indx] = max_length
+#             return memo[indx]
+        
+#         pairs.sort()
+        
+#         max_length = 1
+        
+#         for i in range(len(pairs)):
+#             max_length = max(max_length, dp(i))
+            
+#         return max_length
         
         
