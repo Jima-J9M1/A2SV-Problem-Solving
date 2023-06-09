6e709c7bfbcf1a7dd7cfc045626ad5d354eec893
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-06-09 16:00:19 +0300

    Time: 57 ms (5.51%), Space: 16.4 MB (23.09%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,28 +1,49 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[-1]
         
         
-        memo = {}
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[1],nums[0])
+        curMax = dp[0]
+        
+        for i in range(2, len(nums)):
+            dp[i] = curMax + nums[i]
+            curMax = max(curMax, dp[i - 1])
+            
+            
+        return max(dp)
+        
+        
+        
+        
+        
+        
+        
+        
+#         memo = {}
         
     
-        def dfs(indx):
-            if indx not in memo:
-                if indx >= len(nums):
-                    return 0
+#         def dfs(indx):
+#             if indx not in memo:
+#                 if indx >= len(nums):
+#                     return 0
 
 
 
-                rob = dfs(indx + 2) + nums[indx]
-                not_rob = dfs(indx + 1)
+#                 rob = dfs(indx + 2) + nums[indx]
+#                 not_rob = dfs(indx + 1)
 
 
-                memo[indx] = max(rob, not_rob)
+#                 memo[indx] = max(rob, not_rob)
             
-            return memo[indx]
+#             return memo[indx]
         
             
-        ans = dfs(0)
-        return ans
+#         ans = dfs(0)
+#         return ans
     
             
             
