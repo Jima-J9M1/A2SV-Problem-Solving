b0a56189fdb5b534de1984df6940f64ce534544f
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-06-17 17:32:32 +0300

    Time: 5445 ms (18.51%), Space: 16.5 MB (52.95%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.py
+++ 279-perfect-squares/279-perfect-squares.py
@@ -1,30 +1,40 @@
 class Solution:
     def numSquares(self, n: int) -> int:
         squares = [i * i for i in range(1, 101)]
-        memo = {}
-        length = len(squares)
+        dp = [float("inf")]*(n + 1)
+        dp[0] = 0
+        length = len(dp)
         
-        def dp(amount):
-            if amount == 0:
-                return 0
-            if amount in memo:
-                return memo[amount]
-            
-            if amount < 0:
-                return float("inf")
-            
-            count = float("inf")
-            for i in range(length):
-                if amount - squares[i] < 0:
-                    break
+        for i in range(length):
+            for j in range(len(squares)):
+                if squares[j] <= i:
+                    dp[i] = min(dp[i], 1 + dp[i - squares[j]])
                     
-                count = min(count, 1 + dp(amount - squares[i]))
-                
-            memo[amount] = count
+        return dp[n]
+#         memo = {}
+#         length = len(squares)
+        
+#         def dp(amount):
+#             if amount == 0:
+#                 return 0
+#             if amount in memo:
+#                 return memo[amount]
             
-            return memo[amount]
+#             if amount < 0:
+#                 return float("inf")
+            
+#             count = float("inf")
+#             for i in range(length):
+#                 if amount - squares[i] < 0:
+#                     break
+                    
+#                 count = min(count, 1 + dp(amount - squares[i]))
+                
+#             memo[amount] = count
+            
+#             return memo[amount]
         
-        dp(n)
+#         dp(n)
         
         
-        return memo[n]
+#         return memo[n]
