aea930f28338bd2e0817748d96823248651596d9
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-06-25 09:06:52 +0300

    Time: 49 ms (39.20%), Space: 16.1 MB (95.02%) - LeetHub

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,10 +1,11 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        split_word = haystack.replace(needle, "0")
+#         split_word = haystack.replace(needle, "0")
         
-        for i in range(len(split_word)):
-            if split_word[i] == '0':
-                return i
+#         for i in range(len(split_word)):
+#             if split_word[i] == '0':
+#                 return i
                 
                 
-        return -1
+#         return -1
+        return haystack.find(needle)
