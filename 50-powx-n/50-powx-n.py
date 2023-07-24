a24bd31d79efb7039f747cbb9f2faae2dad40510
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-07-24 21:51:12 +0300

    Time: 41 ms (85.77%), Space: 16.4 MB (7.14%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,7 +1,7 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         
-        #if the number is negative then calculate the power of the abs of the number and return one over the result of the postivie number 
+
         if n < 0 :
             return 1/self.power(x,-1*n)
         
@@ -9,12 +9,10 @@ class Solution:
             
     
     def power(self,x,n):
-        
-        #if the power of the number is zero which return one         
+           
         if n == 0:
             return 1
         
-        #if the power is odd value then calculate with even value and mutiply by one x
         if n % 2 == 1:
             
             ans = self.power(x, n // 2)
