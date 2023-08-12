dbbe7be3b278a3f3edc9e172b530b474e1e2b76b
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-08-12 08:33:33 +0300

    Time: 53 ms (97.43%), Space: 16.9 MB (36.10%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,28 +1,42 @@
 class Solution:
-    def represetative(self,rep, x):
-        while rep[x] != x:
-            x = rep[x]
-            
-        return rep[x]
+    def find(self, rep, node):
+        if rep[node] != node:
+            rep[node] = self.find(rep, rep[node])
+        
+        return rep[node]
     
-    def union(self, rep,x,y):
-        rep_x = self.represetative(rep, x)
-        rep_y = self.represetative(rep, y)
+    def union(self, rep, node1, node2,size):
+        parent_1 = self.find(rep, node1)
+        parent_2 = self.find(rep, node2)
         
-        if rep_x == rep_y:
-            return [x, y]
+        if parent_1 == parent_2:
+            return True
         
-        rep[rep_x] = rep_y
+        else:
+            if size[parent_1] > size[parent_2]:
+                rep[parent_2] = parent_1
+            elif size[parent_2] > size[parent_1]:
+                rep[parent_1] = parent_2
+            else:
+                rep[parent_1] = parent_2
+                size[parent_2] += 1
         
         return False
-    
+        
+        
+        
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        rep = {i : i for i in range(1, len(edges) + 1)}
+        representative = {i:i for i in range(1, len(edges) + 1)} # space O(n)
+        size = [1 for i in range(len(edges) + 1)]
+        graph = defaultdict(list) # space O(n)
         
-        
+#         for start,end in edges:
+#             graph[start].append(end) # Time O(n)
+    
         ans = []
         for v1, v2 in edges:
-            ans = self.union(rep, v1,v2)
+            ans = self.union(representative, v1, v2,size)
+            
             if ans:
-                break
-        return ans
+                return [v1, v2]
+                
