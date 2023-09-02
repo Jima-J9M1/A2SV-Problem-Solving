76a5642c175513c672d0c87a0394d52f5974e09e
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-09-02 20:40:19 +0300

    Time: 241 ms (72.75%), Space: 26.3 MB (5.55%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -1,42 +1,39 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-        #Given: S -> string and dictionary -> list
-        # Break S into one or more non-overlapping substring, each substring is present in dictionary.
-        # There may be some extra characters in s which are not present in any of the substrings.
-        
-        #Goal: return the minimum number of extra characters
-        #Approach
         n = len(s)
-        dictionary_set = set(dictionary)
-        dp = [0] * (n + 1)
-         
-        for start in range(n - 1, -1, -1):
-            dp[start] = 1 + dp[start + 1]
+        root = self.buildTrie(dictionary)
+        
+        print(root)
+        @cache
+        def dp(start):
+            if start == n:
+                return 0
+            # To count this character as a left over character 
+            # move to index 'start + 1'
+            ans = dp(start + 1) + 1
+            node = root
             for end in range(start, n):
-                cur = s[start:end + 1]
-                if cur in dictionary_set:
-                    dp[start] = min(dp[start], dp[end + 1])
-                
-                
-        return dp[0]
-#         memo = {}
-#         n, dictionary_set = len(s), set(dictionary)
-#         # @cache
-#         def dp(start):
-#             if start == n:
-#                 return 0
-#             if start in memo:
-#                 return memo[start]
-#             # To count this character as a left over character 
-#             # move to index 'start + 1'
-#             ans = dp(start + 1) + 1
-#             for end in range(start, n):
-#                 curr = s[start: end + 1]
-#                 if curr in dictionary_set:
-#                     ans = min(ans, dp(end + 1))
-                    
-#             memo[start] = ans
-            
-#             return memo[start]
-            
-#         return dp(0)
+                if s[end] not in node.children:
+                    break
+                node = node.children[s[end]]
+                if node.is_word:
+                    ans = min(ans, dp(end + 1))
+            return ans
+        
+        return dp(0)
+    
+    def buildTrie(self, dictionary):
+        root = TrieNode()
+        for word in dictionary:
+            node = root
+            for char in word:
+                if char not in node.children:
+                    node.children[char] = TrieNode()
+                node = node.children[char]
+            node.is_word = True
+        return root
