f41f0767c7373fd2d8f95948d08f28c0e5fb1ddf
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-09-02 20:17:33 +0300

    Time: 193 ms (92.06%), Space: 16.3 MB (87.30%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -6,24 +6,37 @@ class Solution:
         
         #Goal: return the minimum number of extra characters
         #Approach
-        memo = {}
-        n, dictionary_set = len(s), set(dictionary)
-        # @cache
-        def dp(start):
-            if start == n:
-                return 0
-            if start in memo:
-                return memo[start]
-            # To count this character as a left over character 
-            # move to index 'start + 1'
-            ans = dp(start + 1) + 1
+        n = len(s)
+        dictionary_set = set(dictionary)
+        dp = [0] * (n + 1)
+         
+        for start in range(n - 1, -1, -1):
+            dp[start] = 1 + dp[start + 1]
             for end in range(start, n):
-                curr = s[start: end + 1]
-                if curr in dictionary_set:
-                    ans = min(ans, dp(end + 1))
+                cur = s[start:end + 1]
+                if cur in dictionary_set:
+                    dp[start] = min(dp[start], dp[end + 1])
+                
+                
+        return dp[0]
+#         memo = {}
+#         n, dictionary_set = len(s), set(dictionary)
+#         # @cache
+#         def dp(start):
+#             if start == n:
+#                 return 0
+#             if start in memo:
+#                 return memo[start]
+#             # To count this character as a left over character 
+#             # move to index 'start + 1'
+#             ans = dp(start + 1) + 1
+#             for end in range(start, n):
+#                 curr = s[start: end + 1]
+#                 if curr in dictionary_set:
+#                     ans = min(ans, dp(end + 1))
                     
-            memo[start] = ans
+#             memo[start] = ans
             
-            return memo[start]
+#             return memo[start]
             
-        return dp(0)
+#         return dp(0)
