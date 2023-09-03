347aeb6aafa1c138f6719135b9992c6af19fdb2d
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-09-03 15:07:36 +0300

    Time: 46 ms (32.18%), Space: 16.8 MB (6.76%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,41 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        #Goal: Is to reaturn the number of unique paths from starting cell to end cell
+        #Approach:build the unique path starting from the end to the starting
+        #        Build Grid -> which has (m + 1) and (n + 1) we need to set with 0's another below row and right column to form  unique path
+        #        Form time complexity I use catching with memo or @cache
+        #        Check the bounder of each row and column with function bound()
+        if n == 1 and m == 1:
+            return 1
+        
+        def bound(row, col):
+            return (0 <= row < m) and (0 <= col < n)
+        
+        
+        grid = [[0] * (n + 1) for _ in range(m + 1)]
+        memo = {}
+        def dp(grid, row, col):
+            if row == m - 1and col == n - 1:
+                return 1
+            if (row, col) in memo:
+                return memo[(row, col)]
+            ncol = col + 1
+            nrow = row + 1
+            if bound(nrow, col):
+                down = dp(grid, nrow, col)
+                grid[nrow][col] = down
+            if bound(row, ncol):
+                right = dp(grid, row, ncol)
+                grid[row][ncol] = right
+                
+            grid[row][col] = grid[row][ncol] + grid[nrow][col]
+            memo[(row, col)] = grid[row][col]
+            return memo[(row, col)]
+        
+        dp(grid, 0, 0)
+        
+        return grid[0][0]
+        
+        
+        
+        
