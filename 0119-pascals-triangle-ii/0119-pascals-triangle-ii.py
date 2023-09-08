4648c0ca2325eca3e6015bec3eba49034bcf8201
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-09-08 14:42:21 +0300

    Time: 44 ms (35.87%), Space: 16.3 MB (68.67%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,27 +1,33 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
+        row = [1]
+        for i in range(1, rowIndex+1):
+            # compute the i-th element of the current row
+            element = row[-1] * (rowIndex-i+1) // i
+            row.append(element)
+        return row
         
-        result = [[1]]
+#         result = [[1]]
         
-        def rec(numRows):
-            if numRows == 1:
-                return
+#         def rec(numRows):
+#             if numRows == 1:
+#                 return
             
-            if numRows == 2:
-                result.append([1,1])
-                return [1,1]
+#             if numRows == 2:
+#                 result.append([1,1])
+#                 return [1,1]
             
-            ans = rec(numRows - 1)
-            res = []
+#             ans = rec(numRows - 1)
+#             res = []
             
-            for i in range(len(ans) - 1):
-                res.append(ans[i] + ans[i + 1])
+#             for i in range(len(ans) - 1):
+#                 res.append(ans[i] + ans[i + 1])
             
-            ans = [1] + res + [1]
-            result.append(ans)
+#             ans = [1] + res + [1]
+#             result.append(ans)
                 
                 
-            return ans
+#             return ans
         
-        rec(rowIndex + 1)
-        return result[rowIndex]
+#         rec(rowIndex + 1)
+#         return result[rowIndex]
