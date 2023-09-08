6cf37980e3ccbbdbb232be4132ea3dac7150ddd6
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-09-08 13:43:26 +0300

    Time: 34 ms (93.54%), Space: 16.4 MB (37.62%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,29 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        
+        result = [[1]]
+        
+        def rec(numRows):
+            if numRows == 1:
+                return
+            
+            if numRows == 2:
+                result.append([1,1])
+                return [1,1]
+            
+            ans = rec(numRows - 1)
+            res = []
+            
+            for i in range(len(ans) - 1):
+                res.append(ans[i] + ans[i + 1])
+            
+            ans = [1] + res + [1]
+            result.append(ans)
+                
+                
+            return ans
+        
+        rec(numRows)
+        return result
+            
+            
