b74afff958a53fc6e84eea8449867bf933a66fc9
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-09-19 16:46:50 +0300

    Time: 504 ms (17.06%), Space: 17.2 MB (36.62%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        if n == 1:
+            return 0
+        
+        def dp(inital, final, length):
+            if final > n:
+                return float("inf")
+            
+            if final == n:
+                return length
+            
+            paste = dp(inital, final + inital, length + 1)
+            copy_paste = dp(final, 2 * final, length + 2)
+            
+            return min(copy_paste, paste)
+        
+        
+        ans = dp(1, 2, 2)
+        
+        return ans
