f5107a8e727e5b7d412f94676a76538dc69233b8
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-09-25 19:05:24 +0300

    Time: 1633 ms (43.05%), Space: 41.4 MB (54.39%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,30 +1,19 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        memo = {}
         text1 = s
         text2 = s[::-1]
+        n = len(text1)
+        m = len(text2)
         
-        def dp(i, j):
-            
-            if (i, j) in memo:
-                return memo[(i, j)]
-            
-            if i >= len(text1) or j >= len(text2):
-                return 0
-            
-            
-            if text1[i] == text2[j]:
-                ans = 1 + dp(i + 1, j + 1)
-                memo[(i, j)] = ans
-                
-            else:
-                ans1 = dp(i, j + 1)
-                ans2 = dp(i + 1, j)
-                memo[(i, j)] = max(ans1, ans2)
-            
-            
-            return memo[(i, j)]
-            
-        res = dp(0, 0)
         
-        return res
+        grid = [[0] * (m + 1) for _ in range(n + 1)]
+        
+        
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                if text1[i] == text2[j]:
+                    grid[i][j] = 1 + grid[i + 1][j + 1]
+                else:
+                    grid[i][j] = max(grid[i][j + 1], grid[i + 1][j])
+                    
+        return grid[0][0]
