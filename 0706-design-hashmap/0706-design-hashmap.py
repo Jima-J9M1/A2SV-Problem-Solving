5375d5f17827d55dbbdaa2f8a452dd019b1e5d8a
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-10-04 20:12:09 +0300

    Time: 183 ms (82.51%), Space: 36 MB (14.88%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -1,57 +1,71 @@
-class Node:
-    def __init__(self,val):
-        self.next = None
-        self.val = val
+# class Node:
+#     def __init__(self,val):
+#         self.next = None
+#         self.val = val
 
 class MyHashMap:
-
     def __init__(self):
-        self.head = Node([-1, -1])
-        
+        self.hashmap = []
 
     def put(self, key: int, value: int) -> None:
-        cur = self.head
-        new_node = Node([key, value])
-        
-        if not cur:
-            cur = new_node
-        else:
-            
-            while cur.next and cur.val[0] != key:
-                cur = cur.next
-                
-            if cur.val[0] == key:
-                cur.val[1] = value
-            else:
-                cur.next = new_node
-        
+        if len(self.hashmap) <= key:
+            self.hashmap += [-1] * (key - len(self.hashmap)+1)
+        self.hashmap[key] = value
 
     def get(self, key: int) -> int:
-        cur = self.head
-        
-        while cur:
-            if cur.val[0] == key:
-                return cur.val[1]
-            
-            cur = cur.next
+        return self.hashmap[key] if key < len(self.hashmap) else -1
 
-        return -1
-    
     def remove(self, key: int) -> None:
-        cur = self.head
+        if key < len(self.hashmap):
+            self.hashmap[key] = -1
+
+#     def __init__(self):
+#         self.head = Node([-1, -1])
         
-        if not cur.next:
-            return
+
+#     def put(self, key: int, value: int) -> None:
+#         cur = self.head
+#         new_node = Node([key, value])
+        
+#         if not cur:
+#             cur = new_node
+#         else:
             
-        prev = None
+#             while cur.next and cur.val[0] != key:
+#                 cur = cur.next
+                
+#             if cur.val[0] == key:
+#                 cur.val[1] = value
+#             else:
+#                 cur.next = new_node
+        
+
+#     def get(self, key: int) -> int:
+#         cur = self.head
+        
+#         while cur:
+#             if cur.val[0] == key:
+#                 return cur.val[1]
+            
+#             cur = cur.next
+
+#         return -1
+    
+#     def remove(self, key: int) -> None:
+#         cur = self.head
+        
+#         if not cur.next:
+#             return
+            
+#         prev = None
         
         
-        while cur and cur.val[0] != key:
-            prev = cur
-            cur = cur.next
+#         while cur and cur.val[0] != key:
+#             prev = cur
+#             cur = cur.next
         
-        if cur:
-            prev.next = cur.next
+#         if cur:
+#             prev.next = cur.next
         
     
 # Your MyHashMap object will be instantiated and called as such:
