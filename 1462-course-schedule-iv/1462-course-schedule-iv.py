5b2c1f8cda2190e5763c04dff24946c921f15469
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-10-06 10:21:03 +0300

    Time: 1373 ms (22.37%), Space: 20.2 MB (58.82%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,39 +1,60 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        graph = defaultdict(set)
-        degree = defaultdict(int)
-        preCourse = {i: set() for i in range(numCourses)}
-        courses = deque()
+        distance = [[False] * numCourses for _ in range(numCourses)]
         
-        
-        for v1,v2 in prerequisites:
-            graph[v1].add(v2)
-            degree[v2] += 1
+        for start, end in prerequisites:
+            distance[start][end] = True
             
-        for indx in range(numCourses):
-            if degree[indx] == 0:
-                courses.append(indx)
-                
-        while courses:
-            length = len(courses)
+        for i in range(numCourses):
+            distance[i][i] = 0
             
-            for _ in range(length):
-                course = courses.popleft()
-                for child in graph[course]:
-                    preCourse[child].add(course)
-                    preCourse[child].update(preCourse[course])
-                    
-                    degree[child] -= 1
-                    
-                    if degree[child] == 0:
-                        courses.append(child)
+            
+        for k in range(numCourses):
+            for i in range(numCourses):
+                for j in range(numCourses):
+                    if not distance[i][j]:
+                        distance[i][j] = distance[i][k] and distance[k][j]
                         
-        answer = []
+                        
+        ans = []
         for start, end in queries:
-            if start in preCourse[end]:
-                answer.append(True)
-            else:
-                answer.append(False)
+            ans.append(distance[start][end])
+            
+        return ans
+#         graph = defaultdict(set)
+#         degree = defaultdict(int)
+#         preCourse = {i: set() for i in range(numCourses)}
+#         courses = deque()
+        
+        
+#         for v1,v2 in prerequisites:
+#             graph[v1].add(v2)
+#             degree[v2] += 1
+            
+#         for indx in range(numCourses):
+#             if degree[indx] == 0:
+#                 courses.append(indx)
                 
-        return answer
+#         while courses:
+#             length = len(courses)
+            
+#             for _ in range(length):
+#                 course = courses.popleft()
+#                 for child in graph[course]:
+#                     preCourse[child].add(course)
+#                     preCourse[child].update(preCourse[course])
+                    
+#                     degree[child] -= 1
+                    
+#                     if degree[child] == 0:
+#                         courses.append(child)
+                        
+#         answer = []
+#         for start, end in queries:
+#             if start in preCourse[end]:
+#                 answer.append(True)
+#             else:
+#                 answer.append(False)
+                
+#         return answer
         
