ecaaad5cd1ceadbc58d37cd675d39a509c579bfc
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-10-09 11:48:45 +0300

    Time: 926 ms (60.74%), Space: 35.8 MB (43.77%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -16,30 +16,49 @@ class Solution:
             9:[2,4]
         }
         
-        def dp(num, size, memo):
-            if size == 0:
-                return 1
-            
-            if (num, size) in memo:
-                return memo[(num, size)]
-            
-            ans = 0
-            for neighbor in knight_move[num]:
-                ans += dp(neighbor, size - 1, memo)
+        dp = [[1]*(n + 1) for _ in range(10)]
+    
+        for i in range(1, n + 1):
+            for j in range(10):
+                if j == 5:
+                    continue
+                    
+                ans = 0
+                for neighbor in knight_move[j]:
+                    ans += dp[neighbor][i - 1]
                 
+                dp[j][i] = ans
                 
-            memo[(num, size)] = ans
-            
-            
-        
-            return memo[(num, size)]
-        
-        ans = 0
-        memo = {}
+        res = 0
         
         for key in knight_move:
-            ans += dp(key, n - 1, memo)
+            res += dp[key][n - 1]
+        
+        return res % (10 ** 9 + 7)
+#         def dp(num, size, memo):
+#             if size == 0:
+#                 return 1
+            
+#             if (num, size) in memo:
+#                 return memo[(num, size)]
+            
+#             ans = 0
+#             for neighbor in knight_move[num]:
+#                 ans += dp(neighbor, size - 1, memo)
+                
+                
+#             memo[(num, size)] = ans
+            
+            
+        
+#             return memo[(num, size)]
+        
+#         ans = 0
+#         memo = {}
+        
+#         for key in knight_move:
+#             ans += dp(key, n - 1, memo)
         
         
-        return ans % (10**9 + 7)
+#         return ans % (10**9 + 7)
             
