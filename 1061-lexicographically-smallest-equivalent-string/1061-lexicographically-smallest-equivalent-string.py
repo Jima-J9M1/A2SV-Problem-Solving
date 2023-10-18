f546f4e5b3b01ddbf1bdd25c1586fe94c44403d8
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-10-18 11:08:29 +0300

    Time: 50 ms (38.56%), Space: 16.2 MB (96.02%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -1,52 +1,56 @@
 class Solution:
-    def find(self, str1):
-        if str1 == self.rep[str1]:
-            return str1
+    def find(self, rep, word):
+        root = word
         
-        par_str = self.find(self.rep[str1])
-        self.rep[str1] = par_str
+        while root != rep[root]:
+            root = rep[root]
+            
+        while word != root:
+            parent = rep[word]
+            rep[word] = root
+            word = parent
+            
+        return root
         
-        return par_str
-    
-    def union(self, str1, str2):
-        parent_1 = self.find(str1)
-        parent_2 = self.find(str2)
+    def union(self, rep, ch1, ch2):
+        rep_ch1 = self.find(rep, ch1)
+        rep_ch2 = self.find(rep, ch2)
         
-        if parent_1 != parent_2:
-            if self.size[ord(parent_1) - 97] > self.size[ord(parent_2) - 97]:
-                self.rep[parent_2] = parent_1
-                self.size[ord(parent_1) - 97] += self.size[ord(parent_2) - 97]
-                
-            else:
-                self.rep[parent_1] = parent_2
-                self.size[ord(parent_2) - 97] += self.size[ord(parent_1) - 97]
-                
-                
-        # if parent_1 > parent_2:
-        #     self.rep[parent_1] = parent_2
-        # else:
-        #     self.rep[parent_2] = parent_1
+        if rep_ch1 == rep_ch2:
+            return 
+        
+        if rep_ch1 < rep_ch2:
+            rep[rep_ch2] = rep_ch1
+            
+        else:
+            rep[rep_ch1] = rep_ch2
             
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        self.rep = {chr(i + 97) : chr(i + 97) for i in range(26)}
-        self.size = [0] * 26
+        rep = {i:i for i in range(26)}
+        n = len(s1)
         
-
-        for i in range(len(s1)):
-            self.union(s1[i], s2[i])
+        for i in range(n):
+            if s1[i] != s2[i]:
+                s1_num = ord(s1[i]) - ord('a')
+                s2_num = ord(s2[i]) - ord('a')
+                self.union(rep, s1_num, s2_num)
+                
+                
+        ans = ""
+        for i in range(len(baseStr)):
+            ord_num = ord(baseStr[i]) - ord('a')
+            rep_val = self.find(rep, ord_num)
+            ans += chr(rep_val + ord('a'))
             
-        s = []
-        for ele in baseStr:
-            ptn = self.find(ele)
-            _min = 'z'
-            for i in range(26):
-                if ptn == self.find(chr(i+97)) and chr(i+97) < _min:
-                    _min=chr(i+97)
-                    
-            s.append(_min)
-            
-            
-            
-        return "".join(s)
         
-            
+        
+        return ans
+        
+        
+        
+        
+        
+        
+        
+        
+        
