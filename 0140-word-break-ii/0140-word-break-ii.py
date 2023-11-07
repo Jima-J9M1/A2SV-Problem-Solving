75af4d4025240c37f9d380a05593e5a72702aecb
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-11-07 12:53:48 +0300

    Time: 42 ms (26.49%), Space: 16.3 MB (62.77%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,37 +1,97 @@
+class Trie:
+    def __init__(self):
+        self.kids = {}
+        self.is_end = False
+        
+        
+        
 class Solution:
+    
+    def __init__(self):
+        self.root = Trie()
+        
+        
+    def insert(self, word):
+        cur = self.root
+        
+        for ch in word:
+            if ch not in cur.kids:
+                cur.kids[ch] = Trie()
+                
+            cur = cur.kids[ch]
+            
+        cur.is_end = True
+        
+        
+    def search(self, word):
+        cur = self.root
+        
+        for ch in word:
+            if ch not in cur.kids:
+                return False
+            
+            cur = cur.kids[ch]
+            
+        return cur.is_end
+    
+    
+    def solve(self, s, st, pos, ans):
+        if pos == len(s):
+            ans.append(st)
+            
+        st += " "
+        
+        for i in range(pos, len(s)):
+            val = self.search(s[pos:i + 1])
+            
+            
+            if val:
+                self.solve(s, st + s[pos:i + 1], i + 1, ans)
+            
+                
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         
-        
-        def helper(s, memo, wordDict):
-            if s in memo:
-                return memo[s]
+        for word in wordDict:
+            self.insert(word)
             
-            if not s:
-                return []
-            
-            res = []
-            
-            for word in wordDict:
-                if not s.startswith(word):
-                    continue
-                    
-                if len(s) == len(word):
-                    res.append(word)
-                    continue
-                    
-                    
-                result = helper(s[len(word):], memo, wordDict)
+        ans = []
+        for i in range(len(s)):
+            if self.search(s[:i + 1]):
+                self.solve(s, s[:i +  1], i + 1,ans)
                 
-                for item in result:
-                    item = word + ' ' + item
-                    res.append(item)
+                
+        return ans
+        
+#         def helper(s, memo, wordDict):
+#             if s in memo:
+#                 return memo[s]
+            
+#             if not s:
+#                 return []
+            
+#             res = []
+            
+#             for word in wordDict:
+#                 if not s.startswith(word):
+#                     continue
+                    
+#                 if len(s) == len(word):
+#                     res.append(word)
+#                     continue
+                    
+                    
+#                 result = helper(s[len(word):], memo, wordDict)
+                
+#                 for item in result:
+#                     item = word + ' ' + item
+#                     res.append(item)
                     
             
-            memo[s] = res
+#             memo[s] = res
             
-            return res
+#             return res
         
-        memo = {}
+#         memo = {}
         
-        return helper(s, memo, wordDict)
+#         return helper(s, memo, wordDict)
                 
