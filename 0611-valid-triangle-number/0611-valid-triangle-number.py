b7f94b6f50a2de107500612b38ed65c11682d5b6
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-11-07 15:29:04 +0300

    Time: 745 ms (67.59%), Space: 16.3 MB (56.83%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -1,33 +1,55 @@
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
-        nums.sort()
         
         count = 0
-        for i in range(len(nums)):
-            k = i + 2
+        
+        nums.sort(reverse=True)
+        
+        for i, num in enumerate(nums):
+            left = i + 1
+            right = len(nums) - 1
             
-            if nums[i] != 0:
-                for j in range(i + 1, len(nums)):
-
-                    start = k
-                    end = len(nums) - 1
-
-                    res = None
-
-
-                    while start <= end:
-                        mid = start + (end - start) // 2
-
-                        if nums[i] + nums[j] > nums[mid]:
-                            res = mid
-                            start = mid + 1
-                        else:
-                            end = mid - 1
-
-                    if res:
-                        count += res - j
-                        k = res
-                        
-                        
-                        
+            
+            while left < right:
+                if nums[left] + nums[right] > nums[i]:
+                    count += (right - left)
+                    left += 1
+                    
+                else:
+                    right -= 1
+                    
         return count
+    
+    
+    
+#         nums.sort()
+        
+#         count = 0
+#         for i in range(len(nums)):
+#             k = i + 2
+            
+#             if nums[i] != 0:
+#                 for j in range(i + 1, len(nums)):
+
+#                     start = k
+#                     end = len(nums) - 1
+
+#                     res = None
+
+
+#                     while start <= end:
+#                         mid = start + (end - start) // 2
+
+#                         if nums[i] + nums[j] > nums[mid]:
+#                             res = mid
+#                             start = mid + 1
+#                         else:
+#                             end = mid - 1
+
+#                     if res:
+#                         count += res - j
+#                         k = res
+                        
+                        
+                        
+#         return count
