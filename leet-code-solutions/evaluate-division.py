5689e9506eee909b52e5a5467cb4b89a93bac6f7
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2023-11-16 10:43:40 +0300

    Added solution for Evaluate Division

--- leet-code-solutions/evaluate-division.py
+++ leet-code-solutions/evaluate-division.py
@@ -1,50 +1,64 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        def dijkstra(graph, start):
-            calculation = {node:float("inf") for node in graph}
-            calculation[start] = 1
-            visited = set()
-            prority_queue = [(start, 1)]
+        graph = defaultdict(list)
+        
+        for (start, end), val in zip(equations, values):
+            graph[start].append((end, val))
+            graph[end].append((start, 1/val))
             
-            while prority_queue:
-                current_node, current_val = heappop(prority_queue)
+        
+        def djkstra(graph, start):
+            distance = {i:float("inf") for i in graph}
+            queue = [(start, 1)]
+            distance[start] = 1
+            visited = set()
+            
+            while queue:
+                current_node, current_dis = heappop(queue)
                 
                 if current_node in visited:
                     continue
                     
-                visited.add(current_node)
-                
-                for neighbor, weight in graph[current_node]:
-                    w = current_val * weight
                     
-                    if w < calculation[neighbor]:
-                        calculation[neighbor] = w
-                        heappush(prority_queue, (neighbor, w))
+                for next_node, val in graph[current_node]:
+                    w = val * current_dis
+                    
+                    if w < distance[next_node]:
+                        distance[next_node] = w
+                        heappush(queue, (next_node, w))
                         
-            return calculation
+            
+            return distance
         
-        
-        graph = defaultdict(list)
-        
-        for equ, val in zip(equations, values):
-            start, end = equ
-            graph[start].append((end, val))
-            graph[end].append((start, 1/val))
-           
         ans = []
         for start, end in queries:
             if start not in graph or end not in graph:
                 ans.append(-1)
                 continue
                 
-            cal = dijkstra(graph, start)
+            res = djkstra(graph, start)
             
-            if cal[end] == float('inf'):
+            if res[end] == float("inf"):
                 ans.append(-1)
                 continue
                 
+            ans.append(res[end])
             
-            ans.append(cal[end])
+            
             
         return ans
+        
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
             
